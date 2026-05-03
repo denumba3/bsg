aab5da6e61f7664534db00fdc9b29221e3a014bf
denumba3 <bsg@lawal.org>
2026-05-03 07:23:38 +0100

    Add files via upload

---
+++ pan-africa-news.js
@@ -0,0 +1,376 @@
+const africaData = {
+
+NorthAfrica:{
+ Algeria:[
+ ["Algeria Press Service","https://www.aps.dz"],
+ ["El Watan","https://www.elwatan.com"]
+ ],
+
+ Egypt:[
+ ["Al-Ahram","https://english.ahram.org.eg"],
+ ["Al-Masry Al-Youm","https://www.almasryalyoum.com"]
+ ],
+
+ Libya:[
+ ["Libya Herald","https://www.libyaherald.com"],
+ ["Libyan Observer","https://www.libyanobserver.ly"]
+ ],
+
+ Mauritania:[
+ ["Al-Akhbar","https://alakhbar.info"],
+ ["Sahara Media","https://saharamedias.net"]
+ ],
+
+ Morocco:[
+ ["Le Matin","https://lematin.ma"],
+ ["MAP","https://www.map.ma"]
+ ],
+
+ Sudan:[
+ ["Sudan Tribune","https://sudantribune.com"],
+ ["Sudanese News","https://www.sudanews.net"]
+ ],
+
+ Tunisia:[
+ ["TAP","https://www.tap.info.tn"],
+ ["La Presse","https://lapresse.tn"]
+ ]
+},
+
+WestAfrica:{
+ AES:[
+ ["Sputnik Africa","https://en.sputniknews.africa/search/?query=AES"],
+ ["leFaso","https://www.lefaso.net"],
+ ["Burkina24","https://www.burkina24.com"],
+ ["Maliweb","https://www.maliweb.net"],
+ ["Le Republicain","https://www.maliweb.net"],
+ ["Niamey24","https://www.niameysoir.com"],
+ ["Le Sahel","https://www.lesahel.org"]
+ ],
+
+ Benin:[
+ ["Fraternite","https://www.fraternitebj.info"],
+ ["Le Matinal","https://www.lematinal.bj"]
+ ],
+
+ BurkinaFaso:[
+ ["leFaso","https://www.lefaso.net"],
+ ["Burkina24","https://www.burkina24.com"]
+ ],
+
+ CapeVerde:[
+ ["A Semana","https://www.asemana.cv"],
+ ["Santiago Magazine","https://www.santiagomagazine.cv"]
+ ],
+
+ Gambia:[
+ ["The Point","https://thepoint.gm"],
+ ["Foroyaa","https://foroyaa.net"]
+ ],
+
+ Ghana:[
+ ["Graphic","https://graphic.com.gh"],
+ ["Daily Guide","https://dailyguidenetwork.com"]
+ ],
+
+ Guinea:[
+ ["Guinee24","https://guinee24.com"],
+ ["Conakry Infos","https://www.conakryinfos.com"]
+ ],
+
+ GuineaBissau:[
+ ["O Gologb","https://www.ogologb.com"],
+ ["Novidades","https://novidades.gw"]
+ ],
+
+ IvoryCoast:[
+ ["Abidjan.net","https://www.abidjan.net"],
+ ["Le Patriote","https://www.lepatriote.net"]
+ ],
+
+ Liberia:[
+ ["New Dawn","https://www.newdawnliberia.com"],
+ ["FrontPageAfrica","https://www.frontpageafricaonline.com"]
+ ],
+
+ Mali:[
+ ["Maliweb","https://www.maliweb.net"],
+ ["Le Republicain","https://www.maliweb.net"]
+ ],
+
+ Niger:[
+ ["Niamey24","https://www.niameysoir.com"],
+ ["Le Sahel","https://www.lesahel.org"]
+ ],
+
+ Nigeria:[
+ ["Premium Times","https://www.premiumtimesng.com"],
+ ["Channels TV","https://www.channelstv.com"],
+ ["Leadership","https://leadership.ng"],
+ ["The Nation","https://thenationonlineng.net"],
+ ["ThisDay","https://www.thisdaylive.com"],
+ ["Tribune","https://tribuneonlineng.com"],
+ ["Naija News","https://www.naijanews.com"],
+ ["The Cable","https://www.thecable.ng"],
+ ["Daily Sun","https://www.sunnewsonline.com"],
+ ["Ripples","https://www.ripplesnigeria.com"],
+ ["Sahara Reporters","https://www.saharareporters.com"],
+ ["Vanguard","https://www.vanguardngr.com"],
+ ["Punch","https://www.punchng.com"],
+ ["Daily Trust","https://www.dailytrust.com"]
+ ],
+
+ Senegal:[
+ ["Le Soleil","https://www.lesoleil.sn"],
+ ["Seneweb","https://www.seneweb.com"]
+ ],
+
+ SierraLeone:[
+ ["Concord Times","https://www.concordtimes.com"],
+ ["Sierra Express","https://www.sierraexpressmedia.com"]
+ ],
+
+ Togo:[
+ ["Togo Presse","https://www.togopresse.tg"],
+ ["Le Correcteur","https://lecorrecteur.info"]
+ ]
+},
+
+CentralAfrica:{
+ Angola:[
+ ["Jornal de Angola","https://www.jornaldeangola.ao"],
+ ["O Pais","https://www.opais.co.ao"]
+ ],
+
+ Cameroon:[
+ ["Cameroon Tribune","https://www.cameroon-tribune.cm"],
+ ["CRTV","https://www.crtv.cm"]
+ ],
+
+ CentralAfricanRepublic:[
+ ["Journal de Bangui","https://www.journaldebangui.com"],
+ ["Radio Ndeke Luka","https://www.radiondekeluka.org"]
+ ],
+
+ Chad:[
+ ["Alwihda","https://www.alwihdainfo.com"],
+ ["Le Progres","https://www.leprogres.td"]
+ ],
+
+ Congo:[
+ ["Les Depeches","https://www.lesdepechesdebrazzaville.fr"],
+ ["Congo24","https://www.congo24.net"]
+ ],
+
+ DRCongo:[
+ ["Radio Okapi","https://www.radiookapi.net"],
+ ["Le Potentiel","https://www.lepotentiel.cd"]
+ ],
+
+ Gabon:[
+ ["Gabon24","https://www.gabon24.ga"],
+ ["Nouvelle Republique","https://www.lanouvellerepublique.com"]
+ ],
+
+ EquatorialGuinea:[
+ ["Diario Rombe","https://www.diariorombe.es"],
+ ["Radio Macuto","https://radiomacuto.net"]
+ ],
+
+ SaoTome:[
+ ["Tela Non","https://www.telanon.info"],
+ ["STP24","https://www.stp24.com"]
+ ]
+},
+
+EastAfrica:{
+ Burundi:[
+ ["Iwacu","https://www.iwacu-burundi.org"],
+ ["Renouveau","https://www.renouveau.digital"]
+ ],
+
+ Djibouti:[
+ ["La Nation","https://www.lanation.dj"],
+ ["Djibouti24","https://www.djibouti24.com"]
+ ],
+
+ Eritrea:[
+ ["ENA","http://www.shabait.com"],
+ ["Asmarino","https://www.asmarino.com"]
+ ],
+
+ Ethiopia:[
+ ["Addis Standard","https://addisstandard.com"],
+ ["Reporter","https://www.ethiopianreporter.com"]
+ ],
+
+ Kenya:[
+ ["Nation","https://nation.africa"],
+ ["Capital FM","https://www.capitalfm.co.ke"]
+ ],
+
+ Madagascar:[
+ ["Midi Madagasikara","https://www.midi-madagasikara.mg"],
+ ["L'Express","https://lexpress.mg"]
+ ],
+
+ Malawi:[
+ ["Nation MW","https://www.mwnation.com"],
+ ["Malawi24","https://malawi24.com"]
+ ],
+
+ Mozambique:[
+ ["Noticias","https://www.jornalnoticias.co.mz"],
+ ["O Pais","https://www.opais.co.mz"]
+ ],
+
+ Rwanda:[
+ ["New Times","https://www.newtimes.co.rw"],
+ ["Rwanda Today","https://www.rwandatoday.africa"]
+ ],
+
+ Somalia:[
+ ["Radio Muqdisho","https://www.radiomuqdisho.so"],
+ ["SONNA","https://sonna.so"]
+ ],
+
+ SouthSudan:[
+ ["Juba Monitor","https://www.jubamonitor.com"],
+ ["Radio Tamazuj","https://radiotamazuj.org"]
+ ],
+
+ Tanzania:[
+ ["Citizen","https://www.thecitizen.co.tz"],
+ ["Daily News","https://www.dailynews.co.tz"]
+ ],
+
+ Uganda:[
+ ["New Vision","https://www.newvision.co.ug"],
+ ["Observer","https://www.observer.ug"]
+ ],
+
+ Zambia:[
+ ["Daily Mail","https://www.daily-mail.co.zm"],
+ ["Post","https://www.postzambia.com"]
+ ],
+
+ Zimbabwe:[
+ ["Herald","https://www.herald.co.zw"],
+ ["NewsDay","https://www.newsday.co.zw"]
+ ]
+},
+
+SouthernAfrica:{
+ Botswana:[
+ ["Mmegi","https://www.mmegi.bw"],
+ ["Patriot","https://www.thepatriot.co.bw"]
+ ],
+
+ Lesotho:[
+ ["Lesotho Times","https://lestimes.com"],
+ ["Public Eye","https://publiceyenews.com"]
+ ],
+
+ Namibia:[
+ ["New Era","https://www.ne.com.na"],
+ ["Namibian","https://www.namibian.com.na"]
+ ],
+
+ SouthAfrica:[
+ ["SABC News","https://www.sabcnews.com"],
+ ["News24","https://www.news24.com"],
+ ["Mail & Guardian","https://mg.co.za"]
+ ],
+
+ Eswatini:[
+ ["Observer","https://www.observer.org.sz"],
+ ["Times","https://www.times.co.sz"]
+ ]
+}
+
+};
+
+
+
+function initIntel(){
+
+ const grid=document.getElementById('intelGrid');
+ grid.innerHTML='';
+
+ for(const [region,countries] of Object.entries(africaData)){
+
+   let html=`
+   <div class='region-block'>
+   <div class='region-title'>
+   ${region.replace(/([A-Z])/g,' $1').trim()}
+   </div>
+   `;
+
+   for(const country in countries){
+      html+=`
+      <button
+       class='country-btn'
+       onclick="openCountry('${region}','${country}')">
+       » ${country.replace(/([A-Z])/g,' $1').trim()}
+      </button>
+      `;
+   }
+
+   html+='</div>';
+
+   grid.innerHTML+=html;
+ }
+
+}
+
+
+
+function openCountry(region,country){
+
+ let sources=africaData[region][country];
+
+ document.getElementById(
+ 'modalCountry'
+ ).innerText=
+ 'IDENTIFIED: '+country.toUpperCase();
+
+ let html='';
+
+ sources.forEach(src=>{
+   html+=`
+   <a
+    class='modal-link'
+    href='${src[1]}'
+    target='_blank'>
+    ${src[0]}
+   </a>
+   `;
+ });
+
+ document.getElementById(
+ 'modalLinks'
+ ).innerHTML=html;
+
+ document.getElementById(
+ 'modalOverlay'
+ ).style.display='block';
+
+ document.getElementById(
+ 'newsModal'
+ ).style.display='block';
+
+}
+
+
+function closeModal(){
+
+ document.getElementById(
+ 'modalOverlay'
+ ).style.display='none';
+
+ document.getElementById(
+ 'newsModal'
+ ).style.display='none';
+
+}
+
